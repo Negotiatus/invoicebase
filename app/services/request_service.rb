e69466af72994e9b2f80c58e08f715d5b5ed3472
huyhuynh408 <huy@huywhomustnotbenamed.com>
2017-10-13 17:31:54 -0400

    add params to request url

--- app/services/request_service.rb
+++ app/services/request_service.rb
@@ -1,7 +1,7 @@
 class RequestService
   def self.get_request(vendor_name:, start_date:, end_date:)
     response = HTTParty.get(
-      "#{ENV['NEGOTIATUS_API_URL']}/invoicebase_domains",
+      "#{ENV['NEGOTIATUS_API_URL']}/invoicebase_domains?vendor_name=#{vendor_name}&start_date=#{start_date}&end_date=#{end_date}",
       headers: {
         Authorization: "Secret #{ENV['PARTNER_SECRET']}"
       }
