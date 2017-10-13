d0ffc19a65a25f81df6e01bced38818d65e1c914
huyhuynh408 <huy@huywhomustnotbenamed.com>
2017-10-13 18:03:46 -0400

    move into one service

--- app/services/internal_record_importer_service.rb
+++ app/services/internal_record_importer_service.rb
@@ -1,4 +1,19 @@
 class InternalRecordImporterService
+  def self.get_request(vendor_name:, start_date:, end_date:)
+    response = HTTParty.get(
+      "#{ENV['NEGOTIATUS_API_URL']}/invoicebase_domains?vendor_name=#{vendor_name}&start_date=#{start_date}&end_date=#{end_date}",
+      headers: {
+        Authorization: "Secret #{ENV['PARTNER_SECRET']}"
+      }
+    )
+
+    if response.code == 200
+      JSON.parse(response.body)["orders"].each do |record|
+        import(record: record)
+      end
+    end
+  end
+
   def self.import(record:)
     internal_record = InternalRecord.create(
       reference_number:            record[:nego_order_number],
@@ -9,6 +24,8 @@ class InternalRecordImporterService
       date:                        record[:date]
     )
 
-
+    unless internal_record.persisted?
+      # need to do anything?
+    end
   end
 end
